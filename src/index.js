217f8923675aa89fe6addc93c2a352170a04064a
ale_figure8 <gomeznieto@gmail.com>
2021-12-01 17:31:28 -0300

    Redis config

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ const app = express()
 const axios = require('axios')
 const redis = require('redis')
 const responseTime = require('response-time')
+const { json } = require('express')
 
-redis.createClient({
-    host: 'localhost',
+const client = redis.createClient({
+    host: '127.0.0.1',
     port: 6379
+})
+client.connect()
+client.on('ready', () => {
+    console.log('Redis is connected');
 })
 
 app.set('port', process.env.PORT || 3000)
@@ -15,9 +20,20 @@ app.use(express.json())
 app.use(responseTime())
 
 app.get('/character', async (req, res) => {
+    client.get('characters')
+    .then(response => {
+        return res.json({succes: true, data: JSON.parse(response)})
+    })
+    .catch(err => console.log(err))
+
     const url = "https://rickandmortyapi.com/api/character"
     const character = await axios(url)
-    res.json({success: true, data: character.data})
+
+    client.set('characters', JSON.stringify(character.data))
+    .then(() => {
+        return res.status(200).json({success: true, data: character.data})
+    })
+    .catch(err => console.log(err))
 })
 
 app.all('*', (req, res)=> {
